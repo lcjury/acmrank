a35d0a2261bafcd248bc1a4bdb2ddf7ac192e0c5
lcjury <zeui16@gmail.com>
2016-05-09 02:48:16 -0300

    Implemented color retrieval on views

--- views/partials/studentRank.blade.php
+++ views/partials/studentRank.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 <div class="small-9 columns">
-<h4>{{$student->name}}</h4>
+    <h4{{$student->color()}}>{{$student->name}}</h4>
 </div>
 <div class="small-3 columns">
 <h5>{{$student->accepted}}</h5>
