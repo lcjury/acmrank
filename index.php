43b0a821486796e6fcfa7ea6c03e14fad1c96361
lcjury <zeui16@gmail.com>
2016-10-03 21:41:27 -0300

    Change timestamp by last user submittion

--- index.php
+++ index.php
@@ -15,20 +15,20 @@ function should_update()
     $last_update = Option::find('last_update');
     return $last_update->value + getenv('SCORE_CACHE') < time(); //update is required
 }
+
 function update_score()
 {
     $hunter = new Hunter();
     foreach(Student::all() as $student)
     {
         $new_accepted = 0;
-        $last_submission = $student->last_submission;
         $submissions = $hunter->userSubmissions($student->uva_id, $student->last_submission);
         foreach($submissions as $submission)
         {
             if($submission['verdict'] == \Hunter\Status::ACCEPTED)
             {
                 $problem_id = $submission['problem'];
-                $last_submission = max($last_submission, $submission['time']);
+                $student->last_submission = max($student->last_submission, $submission['id']);
                 if(Solved::where('problem_id', $problem_id)->first() == NULL)
                 {
                     $new_accepted++;
@@ -37,7 +37,6 @@ function update_score()
             }
         }
         $student->accepted = $student->accepted + $new_accepted;
-        $student->last_submission = $last_submission;
         $student->save();
     }
     $last_update = Option::find('last_update');
